7fcd30b4ebdd7348fc04441fec01d37e83aa2996
LohitRamachandra <tripurana.lohit@gmail.com>
2025-07-04 23:31:47 +0530

    Added Patient and Appointment Controllers

---
+++ Controllers/AppointmentController.cs
@@ -0,0 +1,44 @@
+﻿using ClinicMVCApp.Interfaces;
+using ClinicMVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicMVCApp.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        private readonly ILogger<AppointmentController> _logger;
+        private readonly IAppointmentRepository _repo;
+
+
+        public AppointmentController(ILogger<AppointmentController> logger, IAppointmentRepository repo)
+        {
+            _logger = logger;
+            _repo = repo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Book()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Book(Appointment appointment)
+        {
+            if (ModelState.IsValid)
+            {
+                await _repo.AddAsync(appointment);
+                return RedirectToAction("Confirmation");
+            }
+            return View(appointment);
+        }
+
+        public IActionResult Confirmation()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/PatientController.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using ClinicMVCApp.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicMVCApp.Controllers
+{
+    public class PatientController : Controller
+    {
+        private readonly ILogger<PatientController> _logger;
+        private readonly IPatientRepository _repo;
+        public PatientController(ILogger<PatientController> logger, IPatientRepository repo) 
+        {
+            _logger = logger;
+            _repo = repo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var patients = await _repo.GetAllAsync();
+            return View(patients);
+        }
+
+        // Add Create/Edit/Delete actions as needed
+    }
+
+
+}

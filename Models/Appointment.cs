55e602ef637b5781a83950b804e7c0b47508bc65
LohitRamachandra <tripurana.lohit@gmail.com>
2025-07-04 21:44:52 +0530

    Create Models

---
+++ Models/Appointment.cs
@@ -0,0 +1,17 @@
+﻿namespace ClinicMVCApp.Models
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+        public int PatientId { get; set; }
+        public Patient Patient { get; set; }
+
+        public string AppointmentType { get; set; }
+        public DateTime Date { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public string Status { get; set; } // e.g., Confirmed, Pending
+    }
+
+
+}
